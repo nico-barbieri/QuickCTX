42f592ecad7f80ab2b04932bc6a5d5d5144a3672
Nico Barbieri <nicobarbieri.m@gmail.com>
2025-06-27 16:57:45 +0200

    minor bugfixes

--- src/index.js
+++ src/index.js
@@ -1,14 +1,6 @@
 import MenuCommand from './classes/MenuCommand.js';
 import QuickCTX from './classes/QuickCTX.js';
 
-// export the main components. For UMD, these will be `window.QuickCTX.QuickCTX` and `window.QuickCTX.MenuCommand`, for ESM they will be imported directly.
-export { QuickCTX, MenuCommand };
+import "./styles/default.css"; 
 
-// for UMD builds, attach the components to the global window object
-if (typeof window !== 'undefined') {
-    if (!window.QuickCTX) {
-        window.QuickCTX = {};
-    }
-    window.QuickCTX.QuickCTX = QuickCTX;
-    window.QuickCTX.MenuCommand = MenuCommand;
-}
+export { QuickCTX, MenuCommand };
